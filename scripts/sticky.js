1aa34a952269901200bf91d6a460ec5c5b1c892c
dubKelly <js.neeb1780@gmail.com>
2017-03-05 20:56:20 -0500

    double-tap/sticky hover solved

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -1,11 +1,11 @@
 var link = document.getElementsByTagName("a");
 for (var i = 0; i < link.length; i++) {
 	link[i].ontouchend = function() {
-		var child = this;
-		var parent = this.parentNode
-		parent.removeChild(this);
-		setTimeout(function() {
-			parent.appendChild(child);
-		}, 0.1);
+		this.classList.toggle("clicked");
+		console.log(this);
+	}
+	link[i].ontouchstart = function() {
+		this.classList.toggle("clicked");
+		console.log(this);
 	}
 }
