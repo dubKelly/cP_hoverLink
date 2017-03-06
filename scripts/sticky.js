92f4c15e40fa5a9abd75268c8e870e679ea192d5
dubKelly <js.neeb1780@gmail.com>
2017-03-06 10:33:52 -0500

    IIFE

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -1,11 +1,15 @@
-var link = document.getElementsByTagName("a");
-for (var i = 0; i < link.length; i++) {
-	link[i].ontouchend = function() {
-		this.classList.toggle("clicked");
-		console.log(this);
-	}
-	link[i].ontouchstart = function() {
-		this.classList.toggle("clicked");
-		console.log(this);
-	}
-}
+// Un-sticks :hover::after when linking to the same page
+(function() {
+  var link = document.getElementsByTagName("a");
+  for (var i = 0; i < link.length; i++) {
+    link[i].ontouchend = function() {
+      var a = this;
+      setTimeout(function() {
+      	a.classList.add("clicked");
+      }, 300);
+    }
+    link[i].ontouchstart = function() {
+      this.classList.remove("clicked");
+    }
+  }
+}());
